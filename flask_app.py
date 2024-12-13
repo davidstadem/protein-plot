19a7f8b82a8eb907b588361a4afd982a3c7ed92e
David Stadem <david.stadem@gmail.com>
2024-12-13 14:28:50 +0000

    got at least the text reading in

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,29 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+import markdown
 
 app = Flask(__name__)
 
+
+#@app.route('/')
+def hello():
+    return_string = 'Hello! Protein Plot, coming soon...'
+    try:
+        return_string += '\ntrying...'
+        html = readme()
+    except:
+        return_string += '\nfailed!'
+    return return_string
+
 @app.route('/')
-def hello_world():
-    return 'Hello, this is the Protein Plot! Coming soon...'
+def readme():
+    with open('README.md', 'r') as file:
+        content = file.read()
+        html_content = markdown.markdown(content)
+    return html_content
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
