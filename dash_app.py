6d13150e128ddc62c12eec57ad0141f8dbdef26a
David Stadem <david.stadem@gmail.com>
2025-01-16 20:34:05 -0600

    downloading csv, POC working

--- dash_app.py
+++ dash_app.py
@@ -1,14 +1,15 @@
-from dash import dcc, html, Dash
-from dash.dependencies import Input, Output
+from dash import dcc, html, Dash, Input, Output, callback
 import plotly.graph_objects as go
 
 import make_protein_plot as mp
 
 app = Dash(__name__)
 
+csv_string=''
 
-@app.callback(
+@callback(
     Output('contour-plot', 'figure'),
+    Input('protein-plot', 'relayoutData'),
 )
 def update_graph():
     
@@ -18,7 +19,24 @@ def update_graph():
     fig = go.Figure(data=[go.Scatter(x=x, y=y, mode='markers')]) #Invisible trace to prevent empty plot on initial load
 
     df,fig = mp.make_fig_easy()
+    global csv_string
+    csv_string = df.to_csv(index=False, encoding='utf-8')
     return fig
+
+@callback(
+    Output("download-dataframe-csv", "data"),
+    Input("download-csv", "n_clicks"),
+    prevent_initial_call=True,
+)
+def download_csv(n_clicks):
+    import pandas as pd
+    df=pd.DataFrame([0,4,2,5])
+    return dcc.send_data_frame(df.to_csv, "mydataset.csv") #Much simpler and more efficient
+
+    #return dict(content="Hello world!", filename="hello.txt")
+
+    #return dict(content=csv_string, filename="mydataset.csv")
+
 
 fig = update_graph()
 
@@ -44,6 +62,8 @@ app.layout = html.Div([
             'aspectRatio': '16/9', #Or any other desired aspect ratio like 16/9, 4/3, etc.
         }
     ),
+    html.Button("Download CSV Me!", id="download-csv"),
+    dcc.Download(id="download-dataframe-csv"),
     dcc.Markdown(children=markdown_text),  
 ])
 
